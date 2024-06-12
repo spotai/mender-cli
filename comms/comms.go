0adb30b1eb8130a8624d270a700d258918791805
Bich Le <bich.le@spotai.co>
2024-06-12 12:19:17 -0700

    minor refactoring

--- comms/comms.go
+++ comms/comms.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/mendersoftware/mender-cli/client/deployments"
 	"golang.org/x/mod/semver"
+	"os"
 	"sort"
 	"strings"
 )
@@ -19,30 +20,29 @@ func LatestArtifact(list *deployments.ArtifactsList) (string, error) {
 	for i, art := range list.Artifacts {
 		prov, ok := art.ArtifactProvides[versionKey]
 		if ok {
-			if !strings.HasPrefix(prov, "v") {
-				prov = "v" + prov
-			}
+			prov = ensureSemverPrefix(prov)
 			if !semver.IsValid(prov) {
-				fmt.Println("warning:", prov, "is not a valid semver version, skipping...")
+				_, _ = fmt.Fprintln(os.Stderr, "warning:", prov, "is not a valid semver version, skipping...")
 				continue
 			}
 			arts = append(arts, &list.Artifacts[i])
-			fmt.Println("candidate:", list.Artifacts[i].Name)
 		}
 	}
 	if len(arts) == 0 {
 		return "", fmt.Errorf("no comms artifacts found")
 	}
 	sort.Slice(arts, func(i, j int) bool {
-		v1 := arts[i].ArtifactProvides[versionKey]
-		if !strings.HasPrefix(v1, "v") {
-			v1 = "v" + v1
-		}
-		v2 := arts[j].ArtifactProvides[versionKey]
-		if !strings.HasPrefix(v2, "v") {
-			v2 = "v" + v2
-		}
+		v1 := ensureSemverPrefix(arts[i].ArtifactProvides[versionKey])
+		v2 := ensureSemverPrefix(arts[j].ArtifactProvides[versionKey])
 		return semver.Compare(v1, v2) > 0
 	})
 	return arts[0].Name, nil
+}
+
+// Ensures that a version string is prefixed with a 'v'
+func ensureSemverPrefix(s string) string {
+	if !strings.HasPrefix(s, "v") {
+		return "v" + s
+	}
+	return s
 }
